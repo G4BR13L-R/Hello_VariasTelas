2bcb71468ea00d9ceba1db3f3042b3b21a51f0c1
Gabriel Silva <802.239@alunos.unigran.br>
2022-09-29 09:54:15 -0400

    Criado codigo principal

--- app/src/main/java/com/gabriel/hello_variastelas/MainActivity.java
+++ app/src/main/java/com/gabriel/hello_variastelas/MainActivity.java
@@ -2,13 +2,34 @@ package com.gabriel.hello_variastelas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonEnviar;
+    EditText textNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+        textNome = findViewById(R.id.textNome);
+
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SegundaActivity.class);
+
+                intent.putExtra("Nome", textNome.getText().toString());
+
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/gabriel/hello_variastelas/SegundaActivity.java
+++ app/src/main/java/com/gabriel/hello_variastelas/SegundaActivity.java
@@ -3,12 +3,19 @@ package com.gabriel.hello_variastelas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SegundaActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        textView = findViewById(R.id.textView);
+
+        textView.setText("Olá " + getIntent().getStringExtra("Nome"));
     }
 }
